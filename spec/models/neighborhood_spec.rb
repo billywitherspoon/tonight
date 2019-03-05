2604c52dbf609301179c5d3ed616208ef2c5026c
allen-woods <allen.woods@flatironschool.com>
2019-03-04 17:05:25 -0800

    Defining RSpec tests.

--- spec/models/neighborhood_spec.rb
+++ spec/models/neighborhood_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
+
 RSpec.describe Neighborhood, type: :model do
-  test_neighborhood = Neighborhood.new()
-  it 'can be instantiated using Neighborhood#new' do
-    expect(test_neighborhood.class.name).to eq("Neighborhood")
+
+  before do
+    Neighborhood.destroy_all
   end
 
-  it 'has a name' do
-    expect(test_neighborhood).to respond_to(:name)
+  capitol_hill = Neighborhood.create(name: "Capitol Hill")
+
+  describe '#create method' do
+    it 'can create instance of model' do
+      expect(capitol_hill.class.name).to eq("Neighborhood")
+    end
+  end
+
+  describe 'instance' do
+    it 'has a name' do
+      expect(capitol_hill).to respond_to(:name)
+    end
   end
 end
