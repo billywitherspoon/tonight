2644ee51c100f4b4969fa3c4cbba9cde775defa4
github username <github email address>
2019-03-04 17:39:04 -0800

    added views/controllers user and event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,20 @@
 class EventsController < ApplicationController
+   before_action :set_event, only: [:edit, :update, :show]
+   
+   def index 
+      @events = Event.all
+   end 
+
+   def show 
+   end
+   
+   private 
+
+   def set_event
+      @event = Event.find(params[:id])
+   end
+
+   def event_params 
+      params.require(:event).permit(:name, :id)
+   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+   before_action :set_user, only: [:edit, :update, :show]
+   
+   def index 
+      @users = User.all
+   end 
+
+   def new 
+      @user = User.new 
+   end 
+
+   def create 
+   end 
+
+   def show 
+   end 
+
+   def update 
+   end 
+
+   def edit 
+   end 
+
+   def delete 
+   end
+
+   private 
+
+   def set_user
+      @user = User.find(params[:id])
+   end
+
+   def user_params 
+      params.require(:user).permit(:name, :id)
+   end
 end
