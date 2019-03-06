b7e3412dea665212aadc1c19b0209039eb5f39a2
github username <github email address>
2019-03-05 18:27:17 -0800

    created more forms, brought through @user and @event for check_in create

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_many :check_ins
   has_many :events, through: :check_ins
-  
+
   validates :email, {presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }}
   validates :first_name, :last_name, :phone_number, presence: true
   # validates :phone_number, {presense: true, length: }
@@ -12,4 +12,7 @@ class User < ApplicationRecord
   :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
   :message => "- Phone numbers must be in xxx-xxx-xxxx format."
 
+  def full_name
+    self.first_name.titleize + " " + self.last_name.titleize
+  end
 end
