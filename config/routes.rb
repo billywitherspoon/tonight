03a68e3c2c12e2b735e010914811da9cb7f71662
allen-woods <allen.woods@flatironschool.com>
2019-03-05 17:06:10 -0800

    Worked on applications.css

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,6 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
 
-<<<<<<< HEAD
-  # Routes go in here
-  # resources :users, only: [:show, :new, :index, :create]
-  # root 'users#index'
-=======
->>>>>>> 71c611846d655527e0900fc3789928d9c485f9a1
   # get '/login', to: 'sessions#new'
   # post '/login', to: 'sessions#create'
   # delete '/login', to: 'sessions#destroy', as: :logout
