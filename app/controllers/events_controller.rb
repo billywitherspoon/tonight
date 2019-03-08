2a45670a5c4aca4e90cc982e55bcc07cccc61396
github username <github email address>
2019-03-07 18:13:11 -0800

    working on sorting events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
    before_action :logged_in?
    
    def index 
-      @events = Event.all
+      @events = Event.all.sort_by{|event| event.total_avg_rating}
    end 
 
    def show 
